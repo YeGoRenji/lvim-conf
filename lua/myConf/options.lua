a0d242b1c4823d055d275f1cabe9ebf46d33c9c9
YeGoTaChi <yego.designs@gmail.com>
2024-03-05 20:37:15 +0100

    Hot reloading conf / Set Neovide Font to mono

--- lua/myConf/options.lua
+++ lua/myConf/options.lua
@@ -1,12 +1,12 @@
 local options = {
-	shiftwidth = 4,                          		-- the number of spaces inserted for each indentation
-	tabstop = 4,                             		-- insert 4 spaces for a tab
-	expandtab = false,                        		-- dont convert tabs to spaces
-  	termguicolors = true,                    		-- set term gui colors (most terminals support this)
-	relativenumber = true,							-- Relative numbering !
-	smartindent = true,								-- Smart indentation ?
-	guifont = { "JetBrainsMono Nerd Font", ":h20" },	-- the font used in graphical neovim applications
-	showcmd = true,									-- To show commands below ? (I use it to see nb of chars or lines)
+	shiftwidth = 4,											-- the number of spaces inserted for each indentation
+	tabstop = 4,											-- insert 4 spaces for a tab
+	expandtab = false,										-- dont convert tabs to spaces
+  	termguicolors = true,									-- set term gui colors (most terminals support this)
+	relativenumber = true,									-- Relative numbering !
+	smartindent = true,										-- Smart indentation ?
+	guifont = { "JetBrainsMono Nerd Font Mono", ":h20" },	-- the font used in graphical neovim applications
+	showcmd = true											-- To show commands below ? (I use it to see nb of chars or lines)
 }
 
 -- Show Trailing spaces
